3c7433c059de2a0df600ea3ced44859df759bef2
Mihail <MISavelev@stud.kantiana.ru>
2021-12-10 01:27:25 +0200

    add 8 laboratory

---
+++ Laboratory 8/lab8.3.cpp
@@ -0,0 +1,60 @@
+﻿#include<fstream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int N = 100;
+	const int M = 100;
+	setlocale(LC_ALL, "Rus");
+	int** A = new int* [N];
+	for (int i = 0; i < N; i++)
+		A[i] = new int[M];
+	int* Proiz = new int[N];
+	ifstream in("input.txt");
+	int n, m;
+	int maxx = INT_MIN;
+	int i1 = 0;
+	in >> n >> m;
+	//считывание матрицы с файла
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			in >> A[i][j];
+		}
+
+		Proiz[i] = 1;//инициализирование эл-ты произведения 1
+	}
+	//конец считывания
+	//поиск макс. произведения
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			Proiz[i] *= A[i][j];
+		}
+	}
+	//поиск строки с макс.
+	for (int i = 0; i < n; i++) {
+		if (Proiz[i] > maxx) {
+			maxx = Proiz[i];
+			i1 = i;
+		}
+	}
+	//замена строки максимумом
+	for (int j = 0; j < m; j++) {
+		A[i1][j] = maxx;
+	}
+	//вывод матрицы
+	ofstream out("output.txt");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
+			out << A[i][j] << " ";
+		out << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+		delete[] A[i];
+	delete[] A;
+	delete[] Proiz;
+	out.close();
+	return 0;
+}
