29e2a1166e7626c13b887bff2ea0e22062100f4e
Mihail <MISavelev@stud.kantiana.ru>
2021-12-09 23:43:31 +0200

    add Laboratoty №4

---
+++ Laboratory 4/number 6.1/n6lab4.cpp
@@ -0,0 +1,56 @@
+﻿#include<cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int A[10000];
+	cout << " Сколько чисел?" << endl;
+	int n; cin >> n;
+	if (n > 1) {
+		for (int i = 0; i < n; i++) {
+			cin >> A[i];
+			if (A[i] < 1) {
+				cout << "Число не подходят условию" << endl;
+				return 1;
+			}
+		}
+		
+		for (int j = 0; j < n; j++) {
+			int p = 0;
+			int count = 0;
+			for (int d = 2; d <= (sqrt(A[j])+1); d++) {
+				if (A[j] % d == 0) {
+					count++;
+				}
+				if (count >= 1) {
+					p++;
+				}
+			}
+		
+			if (p > 1) {
+				for (int h = 0; h < n; h++) {
+					for (int g = h + 1; g < n; g++)
+						if ((A[h]) > (A[g]))
+						{
+							swap(A[h], A[g]);
+						}
+				}
+
+				for (int i = 0; i < n; i++) {
+					cout << A[i] << " ";
+				}
+				break;
+			}
+
+			else {
+				cout << " Есть простые числа" << endl;
+				break;
+			}
+		}
+	}
+	else
+		cout << "Error";
+
+	return 0;
+}
---
+++ Laboratory 4/number 6.2/n6.2lab4.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Сколько чисел?" << endl;
+    int n; cin >> n;
+    if (n < 1) {
+        int A[1000], sum[1000], P[1000]; //P-произведение
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+            sum[i] = 0;
+            P[i] = 1;
+            int x = A[i];
+            while (x > 0) {
+                sum[i] += x % 10;
+                x /= 10;
+            }
+           
+            while (x > 0) {
+                P[i] *= x % 10;
+                x /= 10;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                if ((sum[i] > sum[j]) || ((sum[i] == sum[j]) && (P[i] > P[j])) ||
+                    ((sum[i] == sum[j]) && (P[i] == P[j]) && (A[i] > A[j]))) {
+                    swap(sum[i], sum[j]);
+                    swap(A[i], A[j]);
+                    swap(P[i], P[j]);
+                }
+            }
+        for (int i = 0; i < n; i++)
+            cout << A[i] << " ";
+    }
+    else
+        cout << "Error" << endl;
+    return 0;
+}
