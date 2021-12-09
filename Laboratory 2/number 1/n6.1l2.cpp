321de9a7401274d2f5dbc7ca411a147352a9b986
Mihail <MISavelev@stud.kantiana.ru>
2021-12-10 01:23:45 +0200

    add 2 laboratory

---
+++ Laboratory 2/number 1/n6.1l2.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Вариант 6.1" << endl << "Ввести натуральные числа A, B и C. Если A меньше B и B больше C, то вывести B+A-C, в противном случае если B кратно C, то вывести B/С-B, в остальных случаях вывести A*B-C" << endl;
+    cout << "Введите A,B,C" << endl;
+    int A, B, C; cin >> A >> B >> C;
+    if ((A < 1) || (B < 1) || (C < 1)) {
+        if ((A < B) && (B > C)) 
+        {
+            cout << B + A - C;
+        }
+        else if ((B % C) == 0)
+        {
+            cout << (B / C - B);
+        }
+        else
+        {
+            cout << (A * B - C);
+        }
+    }
+    else
+    {
+        cout << "Данные не подходят условию.";
+    }
+    return 0;
+}
---
+++ Laboratory 2/number 2/n6.2l2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Вариант 6.2. " << endl << "Ввести натуральное число N, обозначающее номер месяца в году. Используя оператор switch вывести название месяца года. Предусмотреть обработку ошибочного ввода N." << endl;
+    cout << "Введите номер месяца" << endl;
+    int n; cin >> n;
+    switch (n) {
+    case '1' :
+    {
+        cout << "Январь" << endl;
+        break;
+    }
+    case '2' :
+    {
+        cout << "Февраль" << endl;
+        break;
+    }
+    case '3' : 
+    {
+        cout << "Март" << endl;
+        break;
+    }
+    case '4' : 
+    {
+        cout << "Апрель" << endl;
+        break;
+    }
+    case '5' : 
+    {
+        cout << "Май" << endl;
+        break;
+    }
+    case '6' :
+    {
+        cout << "Июнь" << endl;
+        break;
+    }
+    case '7' : 
+    {
+        cout << "Июль" << endl;
+        break;
+    }
+    case '8' : 
+    {
+        cout << "Август" << endl;
+        break;
+    }
+    case '9' : 
+    {
+        cout << "Сентябрь" << endl;
+        break;
+    }
+    case '10' : 
+    {
+        cout << "Октябрь" << endl;
+        break;
+    }
+    case '11' :
+    {
+        cout << "Ноябрь" << endl;
+        break;
+    }
+    case '12' :
+    {
+        cout << "Декабрь" << endl;
+        break;
+    }
+    default: 
+    {
+        cout << "Такого месяца нет";
+        break;
+    }
+    }
+    return 0;
+}
