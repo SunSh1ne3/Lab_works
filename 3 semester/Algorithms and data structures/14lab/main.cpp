870e79789d0c4aa19e6fafdb6d8f6a0166110daa
MIhail Savelev <MISavelev@stud.kantiana.ru>
2023-02-27 01:00:58 +0200

    fixed bugs

--- 3 semester/Algorithms and data structures/14lab/main.cpp
+++ 3 semester/Algorithms and data structures/14lab/main.cpp
@@ -2,120 +2,24 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
+#include <list>
 using namespace std;
-
-class T_List
-{
-public:
-    T_List() 
-    {
-        data = "\0";
-        next = nullptr;
-    }
-    T_List(string str)
-    {
-        data = str;
-        next = nullptr;
-    }
-    void AddToList(T_List*& head, string str)
-    {
-        T_List* p = new T_List;
-        p->data = str;
-        p->next = head;
-        head = p;
-    }
-    bool FindToList(T_List* head, string str)
-    {
-        T_List* p = head;
-        while (p->next != nullptr)
-        {
-            p = p->next;
-            if (p->data == str)
-            {
-                cout << p->data << " Yeah! " << endl;
-                return true;
-            }
-            else
-            {
-                cout << p->data << " No " << endl;
-                return false;
-            }
-        }
-        return false;
-    }
-    T_List* Find(T_List* head, string str)
-    {
-        T_List* p = head;
-        while (p->next != nullptr)
-        {
-            p = p->next;
-            if (p->data == str)
-                return p;
-        }
-    }
-
-    void DelToList(T_List*& head, string str)
-    {
-        T_List* tmp;
-        T_List* p = head;
-        while (p != nullptr)
-        {
-            if (p->next == Find(head, str))
-            {
-                tmp = p->next;
-                p->next = p->next->next;
-                delete tmp;
-            }
-            else
-                p = p->next;
-        }
-    }
-
-    void write(T_List* head)
-    {
-        T_List* p = head;
-        while (p!=NULL)
-        {
-            cout << p->data << " ";
-            p = p->next;
-        }
-        cout << endl;
-    }
-
-    void ClearList(T_List* head)
-    {
-        T_List* tmp;
-        T_List* p = head;
-        while (p->next != nullptr)
-        {
-            tmp = p;
-            p = p->next;
-            delete tmp;
-        }
-    }
-   
-private:
-    T_List* next;
-    string data;
-};
 
 class HashTable
 {
 public:
     HashTable()
     {
-        T_List* p = new T_List;
+        list<string> *p = new list<string>;
         
         for (int i = 0; i < size_table; i++)
-            table[i] = p;
+            table.push_back(*p);
         delete p;
     }
 
     ~HashTable()
     {
-        for (int i = 0; i < size_table; i++)
-            table[i]->ClearList(table[i]);
+
     }
 
     int HashFunction(string str)
@@ -129,8 +33,9 @@ public:
 
     void AddToTable(string str)
     {   
-        table[HashFunction(str)]->AddToList(table[HashFunction(str)], str);
+        table[HashFunction(str)].push_back(str);
     }
+
     void AddInTable()
     {
         ifstream in; in.open("table.txt");
@@ -142,42 +47,33 @@ public:
             if ((vhod == ' ' || vhod == '\n') && !str.empty())
             {
                 AddToTable(str);
-                str = "";
+                str.clear();
             }
             else
                 str += vhod;
         }
+        in.close();
         if (!str.empty())
             AddToTable(str);
-        in.close();
+        
     }
 
-    bool FindToTabel(string str)
-    {
-        if(table[HashFunction(str)]->FindToList(table[HashFunction(str)], str))
-            return true;
-        return false;
-    }
-
-    void DelToTable(string str)
-    {
-        if (FindToTabel(str))
-        {
-           table[HashFunction(str)]->DelToList(table[HashFunction(str)], str);
-        }
-    }
     void Out_Table()
     {   
         for (int i = 0; i < size_table; i++)
         {
-            if (table[i] != nullptr)
-                cout << i + 1 << " -> "; table[i]->write(table[i]);
+            if (!table[i].empty())
+            {
+                cout << i + 1 << " -> ";
+                copy(table[i].begin(), table[i].end(), ostream_iterator<string>(cout, " "));
+                cout << endl;
+            }
         }
     }
 
 private:
     static const int size_table = 37;
-    T_List* table[size_table];
+    vector< list< string > > table;
 };
 int main()
 {
