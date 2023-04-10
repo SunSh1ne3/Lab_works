87957ae4265b6f7b48947f3a65c2ff6f795f849c
Savelev Mihail <MISavelev@stud.kantiana.ru>
2023-04-10 15:03:24 +0200

    add 15 & 16 labs

---
+++ 4 semester/ASD/lab15/main.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Graph
+{
+public:
+	Graph()
+	{
+		int element;
+		ifstream in; in.open("matrix.txt");
+		if (!in)
+			cout << "File! \n";
+		else
+			while(in >> element)
+				Num++;
+		in.close();
+		Num = sqrt(Num);
+		deg = new pair<int, int>[Num];
+		v = new list<int>[Num];
+		B = new vector<int>[Num];
+	}
+	void AddVers()
+	{
+		int element; 
+		ifstream in; in.open("matrix.txt");
+		if (!in)
+			cout << "File! \n"; 
+		else
+		{
+			for (int i = 0; i < Num; i++)
+				for (int j = 0; j < Num; j++)
+				{
+					in >> element;
+					if (element != 0)
+					{
+						v[i].push_back(j);
+						deg[i].second++;
+						deg[i].first = i;
+					}
+				}
+		}
+		in.close();
+		
+		cout << "\n Dano: \n";
+		for (int i = 0; i < Num; i++)
+		{
+			cout << i << ": "; copy(v[i].begin(), v[i].end(),ostream_iterator<int>(cout, " "));
+			cout << endl;
+		}
+	}
+
+	void Sort()
+	{
+		for (int i = 0; i < Num - 1; i++)
+			for (int j = i + 1; j < Num; j++)
+				if (deg[i].second < deg[j].second)
+					swap(deg[i], deg[j]);
+	}
+	void Paint()
+	{
+		int k = 0;
+		B[k].push_back(deg[0].first);
+	Start:
+		for (int i = 1; i < Num; i++)
+		{
+			if (!Color_B(deg[i].first) && !Smegni(deg[i].first, B[k]))
+				B[k].push_back(deg[i].first);
+			else continue;
+		}
+		for (int i = 0; i < Num; i++) {
+			if (!Color_B(i)) {
+				k++;
+				goto Start;
+			}
+			else
+				continue;
+		}
+
+		cout << endl;
+		for (int i = 0; i < Num; i++)
+		{
+			if (!B[i].empty())
+			{
+				cout << char(i+65) << ": ";
+				for (int j = 0; j < B[i].size(); j++)
+					cout << B[i][j] << " ";
+				cout << endl;
+			}
+		}
+	}
+
+protected:
+	bool Smegni(int j, vector<int>B_m)
+	{
+		for(auto u:B_m)
+			for (auto i = v[u].begin(); i != v[u].end(); i++)
+				if (j == *i)
+					return true;
+		return false;
+	}
+	bool Color_B(int u)
+	{
+		for (int i = 0; i < Num; i++)
+			for (int j = 0; j < B[i].size(); j++)
+			{
+				if (B[i][j] == u)
+					return true;
+				else
+					continue;
+			}
+		return false;
+	}
+private:
+	int Num=0;
+	list<int> *v;
+	vector<int> *B;
+	pair<int,int>* deg;
+	
+};
+
+int main()
+{
+	Graph G;
+	G.AddVers();
+	G.Sort();
+	G.Paint();
+	return 0;
+}
+
+
---
+++ 4 semester/ASD/lab16/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int W = 5;
+	pair<int, int> things[] = { make_pair(4, 4000), (make_pair(1, 2500)),make_pair(3, 2000)};
+
+	int** c = new int* [size(things) + 1];
+	for (int i = 0; i <= size(things); i++)
+		c[i] = new int[W+1];
+
+	for (int i = 0; i <= size(things); i++)
+	{
+		for (int j = 0; j <= W; j++)
+		{
+			if (i == 0 || j == 0)
+				c[i][j] = 0;
+
+			else
+			{
+				if (things[i - 1].first > j)
+					c[i][j] = c[i - 1][j];
+
+				else
+					c[i][j] = max(c[i - 1][j], things[i - 1].second + c[i - 1][j - things[i - 1].first]);
+
+			}
+		}
+	}
+
+	for (int i = 0; i <= size(things); i++)
+	{
+		for (int j = 0; j <= W; j++)
+		{
+			if (i == 0 || j == 0)
+				cout << " \t";
+			else
+				cout << c[i][j] << " \t \t";
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
