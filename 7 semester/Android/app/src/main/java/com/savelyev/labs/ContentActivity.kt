66355f0df55e25ed988bd5502aa7e335bc231460
MIhail Savelev <MISavelev@stud.kantiana.ru>
2024-10-22 23:02:09 +0200

    Исправлен баг, когда панель навигации была на страницах авторизации

--- 7 semester/Android/app/src/main/java/com/savelyev/labs/ContentActivity.kt
+++ 7 semester/Android/app/src/main/java/com/savelyev/labs/ContentActivity.kt
@@ -1,6 +1,7 @@
 package com.savelyev.labs
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -16,12 +17,37 @@ class ContentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_content)
 
         setupBottomNavigationBar()
+
+
     }
 
     private fun setupBottomNavigationBar() {
         val bottom_nav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         bottom_nav.setupWithNavController(navController)
+        hideNavBars(bottom_nav)
+    }
+
+    private fun hideNavBars(bottom_nav: BottomNavigationView) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment -> {
+                    bottom_nav.visibility = View.GONE
+                }
+
+                R.id.loginFragment -> {
+                    bottom_nav.visibility = View.GONE
+                }
+
+                R.id.registrationFragment -> {
+                    bottom_nav.visibility = View.GONE
+                }
+
+                else -> {
+                    bottom_nav.visibility = View.VISIBLE
+                }
+            }
+        }
+
     }
 }
